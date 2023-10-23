b91fb49f53e38911671953b0742b2b751803acde
Rafael Garcia <rafaelalejandrog7@gmail.com>
2023-10-23 16:43:06 -0500

    step 19

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get "products/index"
+  # get "products/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   resources :products, only: [:index]
+  resources
   get "/products", to: "products#index"
+  get "/products/:id", to: "products#show", as: "product"
 end
