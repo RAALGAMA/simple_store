d8ddbebb6c4c51a4844a4b1871b893e2da644b7d
Rafael Garcia <rafaelalejandrog7@gmail.com>
2023-11-06 13:27:37 -0600

    Updated customer partial to use image_tag helper

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,18 +1,28 @@
 ActiveAdmin.register Customer do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :full_name, :phone_number, :email_address, :notes
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:full_name, :phone_number, :email_address, :notes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :full_name, :phone_number, :email_address, :notes, :image
+
+  index do
+    selectable_column
+    id_column
+    column :full_name
+    column :phone_number
+    column :email_address
+    actions
+  end
+
+  filter :full_name
+  filter :email_address
+
+  form do |f|
+    f.inputs do
+      f.input :full_name
+      f.input :phone_number
+      f.input :email_address
+      f.input :notes
+      f.input :image, as: :file
+    end
+    f.actions
+  end
+
 end
